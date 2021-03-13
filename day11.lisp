6c267142ebec5d9888b4950e7bb0a361c44e1b69
Franck YVONNET <franck@yvonnet.fr>
2021-03-13 20:48:52 +0100

    refactoring

--- day11.lisp
+++ day11.lisp
@@ -1,29 +1,25 @@
 (defpackage :day11
-  (:use :cl :aoc-misc :trivia)
+  (:use :cl :aoc-misc)
   (:import-from :cl-ppcre :split)
   (:import-from :serapeum :nlet)
+  (:import-from :fset :convert :lookup)
   (:export main))
 
 (in-package :day11)
 
 
 (defun main ()
-  (nlet rec ((directions (split "," (car (read-input-as-list 11)))) (coord '(0 0 0)) (furthest 0))
-    (let ((distance (/ (apply #'+ (mapcar #'abs coord)) 2)))
-      (match directions
-        ('() (format t "~a~%~a~%" distance furthest))
-        ((cons first-dir rest-dir)
-         (rec
-           rest-dir
-           (mapcar
-             #'+
-             coord
-             (cond
-               ((string= first-dir "n" ) '( 1   0  -1))
-               ((string= first-dir "ne") '( 1  -1   0))
-               ((string= first-dir "se") '( 0  -1   1))
-               ((string= first-dir "s" ) '(-1   0   1))
-               ((string= first-dir "sw") '(-1   1   0))
-               ((string= first-dir "nw") '( 0   1  -1))))
-           (if (> distance furthest) distance furthest)))))))
+  (let
+    ((dirmap
+       (convert
+         'fset:map
+         '(("n" 1 0 -1) ("ne" 1 -1 0) ("se" 0 -1 1) ("s" -1 0 1) ("sw" -1 1 0) ("nw" 0 1 -1)))))
+    (nlet rec ((directions (split "," (car (read-input-as-list 11)))) (coord '(0 0 0)) (furthest 0))
+      (let ((distance (/ (apply #'+ (mapcar #'abs coord)) 2)))
+        (if (null directions)
+          (format t "~a~%~a~%" distance furthest)
+          (rec
+            (cdr directions)
+            (mapcar #'+ coord (lookup dirmap (car directions)))
+            (if (> distance furthest) distance furthest)))))))
 
