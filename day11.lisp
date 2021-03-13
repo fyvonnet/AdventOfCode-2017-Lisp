7e56af5686ca400100ea41ca871f8a120bd9c61c
Franck YVONNET <franck@yvonnet.fr>
2021-03-13 19:36:08 +0100

    refactoring

--- day11.lisp
+++ day11.lisp
@@ -1,23 +1,32 @@
 (defpackage :day11
   (:use :cl :aoc-misc :trivia)
   (:import-from :cl-ppcre :split)
-  (:import-from :serapeum :nlet)
   (:export main))
 
 (in-package :day11)
 
+
+(defun distance (coord)
+  (/ (apply #'+ (mapcar #'abs coord)) 2))
+
 (defun main ()
-  (nlet rec ((lst (split "," (car (read-input-as-list 11)))) (x 0) (y 0) (z 0) (furthest 0))
-    (let ((dist (/ (+ (abs x) (abs y) (abs z)) 2)))
-      (match lst
-        (nil (format t "~a~%~a~%" dist furthest))
-        ((cons fst rst)
-         (let* ((new-furthest (if (> dist furthest) dist furthest)))
-           (cond
-             ((string= fst "n" ) (rec rst (1+ x)     y  (1- z) new-furthest))
-             ((string= fst "ne") (rec rst (1+ x) (1- y)     z  new-furthest))
-             ((string= fst "se") (rec rst     x  (1- y) (1+ z) new-furthest))
-             ((string= fst "s" ) (rec rst (1- x)     y  (1+ z) new-furthest))
-             ((string= fst "sw") (rec rst (1- x) (1+ y)     z  new-furthest))
-             ((string= fst "nw") (rec rst     x  (1+ y) (1- z) new-furthest)))))))))
+  (destructuring-bind (furthest . coord)
+    (reduce 
+      (lambda (data direction)
+        (destructuring-bind (furthest . coord) data
+          (cons
+            (let ((dist (distance coord))) (if (> dist furthest) dist furthest))
+            (mapcar
+              #'+
+              coord
+              (cond
+                ((string= direction "n" ) '( 1  0 -1))
+                ((string= direction "ne") '( 1 -1  0))
+                ((string= direction "se") '( 0 -1  1))
+                ((string= direction "s" ) '(-1  0  1))
+                ((string= direction "sw") '(-1  1  0))
+                ((string= direction "nw") '( 0  1 -1)))))))
+      (split "," (car (read-input-as-list 11)))
+      :initial-value '(0 0 0 0))
+    (format t "~a~%~a~%" (distance coord) furthest)))
 
