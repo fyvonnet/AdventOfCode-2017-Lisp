9217c94326c31755e385b62a779d7cac9296af0f
Franck YVONNET <franck@yvonnet.fr>
2021-03-16 11:19:08 +0100

    part 2

--- day13.lisp
+++ day13.lisp
@@ -1,60 +1,36 @@
 (defpackage :day13
   (:use :cl :aoc-misc)
   (:import-from :cl-ppcre :scan-to-strings)
-  (:import-from :serapeum :nlet)
   (:import-from :trivia :match)
   (:export main))
 
 (in-package :day13)
 
-(defstruct scanner range (position 1) (moving-forward t))
-
 (defun parse-line (line)
   (multiple-value-bind (_ matches) (scan-to-strings "^(\\d+): (\\d+)$" line)
-    (cons 
-      (parse-integer (aref matches 0))
-      (parse-integer (aref matches 1)))))
+    (destructuring-bind (layer range) (mapcar (lambda (i) (parse-integer (aref matches i))) '(0 1))
+      (list layer range (* 2 (1- range))))))
 
-(defun create-layers (scanners)
-  (let*
-    ((nlayers (1+ (apply #'max (mapcar #'car scanners))))
-     (layers (make-array (list nlayers) :initial-element nil)))
-    (nlet rec ((scans scanners))
-      (if (null scans)
-        layers
-        (destructuring-bind (layer . range) (car scans)
-          (setf (aref layers layer) (make-scanner :range range))
-          (rec (cdr scans)))))))
+(defun severity (layer &optional (delay 0))
+  (destructuring-bind (layer-num range divisor) layer
+    (when (zerop (rem (+ delay layer-num) divisor)) (* layer-num range))))
 
-(defun update-scanners (layers)
-  (loop
-    for l across layers
-    do (when l
-         (if (scanner-moving-forward l)
-           (if (= (scanner-range l) (scanner-position l))
-             (progn
-               (decf (scanner-position l))
-               (setf (scanner-moving-forward l) nil))
-             (incf (scanner-position l)))
-           (if (= 1 (scanner-position l))
-             (progn
-               (incf (scanner-position l))
-               (setf (scanner-moving-forward l) t))
-             (decf (scanner-position l)))))))
+(defun undetected-p (firewall delay)
+  (cond 
+    ((null firewall) t)
+    ((null (severity (car firewall) delay))
+     (undetected-p (cdr firewall) delay))))
 
-(defun cross-firewall (layers)
-  (let ((len (length layers)))
-    (nlet rec ((layer-num 0) (score 0))
-      (if (= layer-num len)
-        score
-        (let*
-          ((layer (aref layers layer-num))
-           (new-score
-             (if (or (null layer) (> (scanner-position layer) 1))
-               score
-               (+ score (* layer-num (scanner-range layer))))))
-          (update-scanners layers)
-          (rec (1+ layer-num) new-score))))))
+(defun find-delay (firewall &optional (delay 1))
+  (if (undetected-p firewall delay)
+    delay
+    (find-delay firewall (1+ delay))))
 
 (defun main ()
-  (print (cross-firewall (create-layers (read-input-as-list 13 #'parse-line)))))
+  (let ((input (read-input-as-list 13 #'parse-line)))
+    (print
+      (reduce
+        (lambda (sum layer)
+          (match (severity layer) (nil sum) (s (+ sum s))))
+        input :initial-value 0))
+    (print (find-delay input))))
